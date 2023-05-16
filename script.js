8f93df6c74620767ced608fa011f54a430b4fe72
kiraGit67 <kirsten@sais.de>
2023-05-16 16:55:07 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+Vue.createApp({
+  data() {
+    return {
+      title: "Let's Whack A Mole",
+      buttonText: "Start New Game",
+      seconds: 0,
+      score: 0,
+      intervalID: null,
+      startButtonDisabled: false,
+      moleButtonDisabled: true,
+      randomLeftNumber: 0.1,
+      randomTopNumber: 0.05,
+    };
+  },
+  methods: {
+    startGame() {
+      this.score = 0;
+      this.seconds = 15;
+      this.startButtonDisabled = true;
+      this.moleButtonDisabled = false;
+
+      this.intervalID = setInterval(() => {
+        this.seconds--;
+        this.randomLeftNumber = Math.random();
+        this.randomTopNumber = Math.random();
+      }, 1000);
+    },
+    whack() {
+      if (this.seconds > 0) {
+        this.score++;
+      }
+    },
+  },
+  computed: {
+    moveMoleStyle() {
+      return `left: ${100 * this.randomLeftNumber}%; top: ${
+        100 * this.randomTopNumber
+      }%;`;
+    },
+  },
+  watch: {
+    seconds(newValue, oldValue) {
+      if (oldValue === 1 && newValue === 0) {
+        clearInterval(this.intervalID);
+        this.moleButtonDisabled = true;
+        this.startButtonDisabled = false;
+      }
+    },
+  },
+}).mount("#app");
